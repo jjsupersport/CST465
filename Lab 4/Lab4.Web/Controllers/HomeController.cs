da24c8d37bed61cc99f055757c4242db0febbf72
unknown <jordan.lewis@oit.edu>
2024-10-17 16:35:51 -0700

    Lab 4 Finished

--- Lab 4/Lab4.Web/Controllers/HomeController.cs
+++ Lab 4/Lab4.Web/Controllers/HomeController.cs
@@ -16,14 +16,23 @@ public class HomeController : Controller
     public IActionResult Laborers()
     {
         ChoreWorkforce myWorkforce = new ChoreWorkforce();
-        // myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Bob", Age = 7, Difficulty = 5 });
-        // myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Alex", Age = 10, Difficulty = 4 });
-        // myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Calvin", Age = 12, Difficulty = 6 });
-        // myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Dee", Age = 9, Difficulty = 3 });
+        myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Bob", Age = 7, Difficulty = 5 });
+        myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Alex", Age = 10, Difficulty = 4 });
+        myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Calvin", Age = 12, Difficulty = 6 });
+        myWorkforce.Laborers.Add(new ChoreLaborer() { Name = "Dee", Age = 9, Difficulty = 3 });
         myWorkforce.AddRandomLaborers(30);
-        var filteredlaborers = myWorkforce.Laborers.Where(laborer => laborer?.Age >= 3 && laborer?.Age <= 10 && laborer?.Difficulty <= 7).OrderBy(laborer => laborer?.Name).ToList();
-        return View(filteredlaborers);
+        var filteredlaborers = myWorkforce.Laborers
+        .Where(laborer => laborer?.Age >= 3 && laborer?.Age <= 10 && laborer?.Difficulty <= 7)
+        .OrderBy(laborer => laborer?.Name)
+        .ToList();
 
+        var filteredworkforce = new ChoreWorkforce();
+        foreach(var laborer in filteredlaborers)
+        {
+            filteredworkforce.Laborers.Add(laborer);
+        }
+
+        return View(filteredworkforce);
     }
 
 }
